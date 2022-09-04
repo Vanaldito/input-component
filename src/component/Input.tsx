eebc05fdffdfe99d4347394360ef4ad0ec112c27
Vanaldito <fredimanuelb@gmail.com>
2022-09-03 18:28:33 -0600

    feat: style the input component

--- src/component/Input.tsx
+++ src/component/Input.tsx
@@ -1,8 +1,14 @@
+import "./Input.css";
+
 export default function Input({
   ...inputAttributes
 }: React.DetailedHTMLProps<
   React.InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
 >) {
-  return <input {...inputAttributes} />;
+  const className = inputAttributes.className
+    ? `${inputAttributes.className} input`
+    : "input";
+
+  return <input {...inputAttributes} className={className} />;
 }
